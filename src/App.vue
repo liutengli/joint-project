f1c43bb93f1111b932d6ac1ac66d7309ffa7ef81
liuzihan123 <2234237217@qq.com>
2019-03-26 16:59:53 +0800

    远程下载前删除底部按钮

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@
     <!-- 用来放各个组件 -->
     <router-view/>
 
-    <!-- 手机底部按钮 -->
-    <div class="nav-bar">
-      <!-- active表示当前tabbar选中的索引 -->
-      <van-tabbar v-model="active" active-color='blue'>
-        <van-tabbar-item icon="home-o" :to="{name:'Home'}">首页</van-tabbar-item>
-        <van-tabbar-item icon="hot-o" dot :to="{name:'List'}">热卖</van-tabbar-item>
-        <van-tabbar-item icon="shopping-cart-o" info="5"  :to="{name:'ShopCart'}">购物车</van-tabbar-item>
-        <van-tabbar-item icon="manager-o" :to="{name:'UserCenter'}">我</van-tabbar-item>
-      </van-tabbar>
-    </div>
+   
 
   </div>
 </template>
@@ -22,7 +13,7 @@
 export default {
   data(){
    return{
-     active:0
+    
    }
  }
 }
